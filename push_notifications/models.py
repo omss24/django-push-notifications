b9fefd879035b2e03eefce44b4a22ea64674b679
Omar Gonzalez <omar@enull.com.mx>
2015-10-05 19:15:54 -0500

    Lucy update

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -6,13 +6,14 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from .fields import HexIntegerField
 
+# from consultant.models import ConsultantLogin
 
 @python_2_unicode_compatible
 class Device(models.Model):
 	name = models.CharField(max_length=255, verbose_name=_("Name"), blank=True, null=True)
 	active = models.BooleanField(verbose_name=_("Is active"), default=True,
 		help_text=_("Inactive devices will not be sent notifications"))
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+	user = models.ForeignKey("consultant.ConsultantToken", blank=True, null=True)
 	date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
 
 	class Meta:
